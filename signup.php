ab0ab4fdcb129105eeee3d55203b540ecdd3e13e
mukeshsihag <sihagmukesh22@gmail.com>
2018-08-16 23:02:44 +0530

    loginsystem

---
+++ signup.php
@@ -0,0 +1,22 @@
+<?php
+    include_once 'header.php';
+?>
+
+<section class="main-container">
+	<div class="main-wrapper">
+		<h2>Signup</h2>
+		<form class="signup-form">
+			<input type="text" name="first" placeholder="Firstname">
+			<input type="text" name="last" placeholder="lastname">
+			<input type="text" name="email" placeholder="E-mail">
+			<input type="text" name="uid" placeholder="user-id">
+			<input type="password" name="pwd" placeholder="password">
+			<button type="Submit">Submit</button>
+		</form>
+	</div>
+</section>
+
+<?php
+    include_once 'footer.php';
+?>
+
